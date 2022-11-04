2bbaef5213bd77c2374678998c112a3931f92ddc
Raptor K <findme@raptor.hk>
2022-11-04 17:07:40 +0800

    updated fakepng_removebg()

--- imgtoolkit/tools.py
+++ imgtoolkit/tools.py
@@ -2,15 +2,19 @@ import dhash
 import glob
 import os
 import cv2
-import numpy
+import numpy as np
 import warnings
-from PIL import Image
+import sys
+from math import ceil
+from PIL import Image, ImageDraw
 from timeit import default_timer as timer
 from datetime import timedelta
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Manager
 from alive_progress import alive_bar
 from itertools import chain
+from pathlib import Path
+from importlib.metadata import version
 
 warnings.simplefilter('ignore', Image.DecompressionBombWarning)
 
@@ -18,7 +22,6 @@ IMG_FILTER = '*.jpg'
 FOLDER_DUP = 'duplicate/'
 FOLDER_BLUR = 'blur/'
 DUP_PREFIX = 'DUPLICATED_'
-VERSION = '0.0.5'
 
 # Main Functions
 def find_duplicate(folder: str = FOLDER_DUP, prefix: str = DUP_PREFIX):
@@ -59,7 +62,7 @@ def find_blur(folder: str = FOLDER_BLUR, threshold: int = 20):
             for i in imgs:
                 img = cv2.imread(i)
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                val = numpy.max(cv2.convertScaleAbs(cv2.Laplacian(gray, 3)))
+                val = np.max(cv2.convertScaleAbs(cv2.Laplacian(gray, 3)))
                 if(val < threshold):
                     cnt += 1
                     os.rename(i, folder + i)
@@ -72,20 +75,119 @@ def find_blur(folder: str = FOLDER_BLUR, threshold: int = 20):
 
 
 def analyze_blur(target_folder: str = '.'):
-    print("Start analyzing blur at path " + target_folder)
     start = timer()
-    
-    imgs = glob.glob(IMG_FILTER, root_dir=target_folder)
+
+    path = Path(target_folder, IMG_FILTER)
+    print("Start analyzing blur at path: " + target_folder)
+    imgs = glob.glob(path.as_posix())
     with alive_bar(len(list(imgs))) as bar:
         for i in imgs:
             img = cv2.imread(i)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            val = numpy.max(cv2.convertScaleAbs(cv2.Laplacian(gray, 3)))
-            print(i, "Blur Value: " + val)
+            val = np.max(cv2.convertScaleAbs(cv2.Laplacian(gray, 3)))
+            print(str(i), "Blur Value: " + str(val))
             bar()
-            
+
     end = timer()
     print_elapsed(end-start)
+
+def fakepng_removebg(imgpath, savepath):
+    if not os.path.exists(imgpath):
+        raise ValueError("The input file does not exist, or cannot be read.")
+
+    # ========================
+    # Step 1: Detect Tile Size
+    # ========================
+    # TODO: Read from command line / input file name
+    original = cv2.imread(imgpath, cv2.IMREAD_UNCHANGED)
+    h, w, *_ = original.shape
+
+    # get the left top pixel color
+    color = original[0][0]
+
+    checker_size_x = 0
+    checker_size_y = 0
+
+    offset_x = 0
+    offset_y = 0
+    if (color == [255, 255, 255]).all(): # start with white color
+        checker_color1 = tuple(color) # white
+    else:
+        checker_color2 = tuple(color)
+
+    for x in range(w):
+        next_color = original[0][x]
+        if next_color[0] != color[0]:
+            # color changed
+            checker_size_x = x
+            offset_x = x
+            break
+
+    for y in range(h):
+        next_color = original[y][0]
+        if next_color[0] != color[0]:
+            # color changed
+            checker_size_y = y
+            offset_y = y
+            if checker_color1 == (255, 255, 255):
+                checker_color2 = tuple(next_color)
+            else:
+                checker_color1 = tuple(next_color)
+            break
+
+    # if the checker is a square
+    if checker_size_x == checker_size_y:
+        # ========================
+    	# Step 2: Generate Tilemap
+    	# ========================
+        image_w = ceil(w / checker_size_x) * checker_size_x
+        image_h = ceil(h / checker_size_x) * checker_size_x
+
+        checkerboard = Image.new("RGB", (image_w, image_h), (255, 0, 0))
+        pixels = checkerboard.load()
+
+        for i in range (0, image_w, checker_size_x):
+            for j in range(0, image_h, checker_size_x):
+                y, x = i // checker_size_x, j // checker_size_x
+                if (y&1)^(x&1):
+                    for di in range(checker_size_x):
+                        for dj in range(checker_size_x):
+                            pixels[i+di, j+dj] = checker_color2
+                else:
+                    for di in range(checker_size_x):
+                        for dj in range(checker_size_x):
+                            pixels[i+di, j+dj] = checker_color1
+
+        checkerboard = checkerboard.crop((0, 0, w, h))
+        checkerboard = checkerboard.resize((w, h))
+
+    	# ========================
+    	# Step 3: Subtract Tilemap
+    	# ========================
+        # convert Pillow image to OpenCV format
+        tiled = cv2.cvtColor(np.array(checkerboard), cv2.COLOR_RGB2BGR)
+        subtract = cv2.subtract(tiled, original)
+
+    	# inverting the color
+        subtract = ~subtract
+        #cv2.imwrite('subtract.png', subtract)
+
+    	# ========================
+    	# Step 4: Add Alpha Channel
+    	# ========================
+        # TODO: Make the colors optional input parameters
+        # The color range is commonly used in the checkboard of fake transparent PNGs
+        mask = cv2.inRange(subtract, (253, 253, 253), (255, 255, 255))
+        #cv2.imwrite('mask.png', mask)
+
+    	# convert the source to RGBA Color Space to enable Alpha channel
+        rgba = cv2.cvtColor(original, cv2.COLOR_RGB2RGBA)
+        rgba[mask > 0] = (0, 0, 0, 0)
+
+    	# Writing and saving the final results to a new image
+        cv2.imwrite(savepath, rgba)
+    else:
+        raise ValueError("Checker size is not square. Please check.")
 
 
 # Support Functions
@@ -125,14 +227,19 @@ def makehash(t):
 
 def print_elapsed(sec):
     print("Elapsed Time: ", timedelta(seconds=sec))
-    
-def version():
-    print(VERSION)
+
+
+def show_version():
+    print("imgtoolkit " + version("imgtoolkit"))
 
 
 def main():
     print("Image Toolkit loaded")
 
+def exception_handler(exception_type, exception, traceback):
+    print(exception_type.__name__,": ", exception)
 
+# default exception handler
+sys.excepthook = exception_handler
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'Image tools for checking photos for blur and duplicates'
 LONG_DESCRIPTION = 'An image tool Python package for checking photos for blur and duplicates'
 
