e2f4be45f133421e754c05f6a495ff0c47ed30e6
Raptor K <findme@raptor.hk>
2022-11-18 12:57:10 +0800

    Update to v0.0.7, added requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6'
-DESCRIPTION = 'Image tools for checking photos for blur and duplicates'
-LONG_DESCRIPTION = 'An image tool Python package for checking photos for blur and duplicates'
+VERSION = '0.0.7'
+DESCRIPTION = 'Collection of image tools for checking photos for blur and duplicates, and handle fake PNG transparency'
+LONG_DESCRIPTION = 'A collection of image tools for checking photos for blur and duplicates, and handle fake PNG transparency'
 
 setup(
         name='imgtoolkit',
@@ -15,7 +15,7 @@ setup(
         packages=find_packages(),
         install_requires=['dhash', 'alive-progress', 'Pillow', 'opencv-python', 'about-time', 'grapheme'],
 
-        keywords=['image', 'find duplicate', 'find blur'],
+        keywords=['image', 'find duplicate', 'find blur', 'fake png'],
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
